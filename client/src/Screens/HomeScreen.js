43fa7aeeaa3e211bd557f1672f7c500815ee4292
burhan <2019cs626@student.uet.edu.pk>
2022-05-05 18:54:05 +0500

    Removed space

--- client/src/Screens/HomeScreen.js
+++ client/src/Screens/HomeScreen.js
@@ -23,7 +23,6 @@ const HomeScreen = () =>
                                     commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
                                     dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
                                     culpa qui officia deserunt mollit anim id est laborum.
-
                                 </span>
                             </p>
                             <a href='true' title="buy button">Start Building Today</a>
